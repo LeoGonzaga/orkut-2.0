c9e4cf7f746e3b1066c6ca4460d4089b1bf6ec6a
LeoGonzaga <sacredlotus05@gmail.com>
2022-09-03 19:46:18 -0300

    feat: adding initial files and create login folder

---
+++ src/pages/Login/components/Forms/Forms.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Input from "../../../../components/Input";
+
+export const Forms: React.FC = () => {
+  return (
+    <div>
+      <p>Junte-se a mais de 20 milhões de pessoas</p>
+
+      <span>Ja tem conta? Só preencher e entrar</span>
+
+      <form action="">
+        <Input />
+      </form>
+    </div>
+  );
+};
